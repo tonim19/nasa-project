9f1c46cb1f32a5f276aa049a4d7211da49df3628
Toni <toni@pop-os.localdomain>
2022-02-13 22:39:03 +0100

    Final

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,18 +1,44 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
-  destination: "Kepler-442 b",
-  customer: ["ZTM", "NASA"],
+  target: "Kepler-442 b",
+  customers: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
 };
 
 launches.set(launch.flightNumber, launch);
 
+export function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
+
 export function getAllLaunches() {
   return Array.from(launches.values());
+}
+
+export function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ["ZTM", "NASA"],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
+export function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
 }
--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { httpGetAllLaunches } from "./launches.controller.js";
+import {
+  httpGetAllLaunches,
+  httpAddNewLaunch,
+  httpAbortLaunch,
+} from "./launches.controller.js";
 
 const launchesRouter = express.Router();
 
 launchesRouter.get("/launches", httpGetAllLaunches);
+
+launchesRouter.post("/launches", httpAddNewLaunch);
+
+launchesRouter.delete("/launches/:id", httpAbortLaunch);
 
 export default launchesRouter;
